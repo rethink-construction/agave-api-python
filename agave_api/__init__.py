520e5847317e14dafccf74ff61bf1d14b9a4e6e8
Joe Petrantoni <79169021+jpetrantoni@users.noreply.github.com>
2024-07-09 03:17:14 -0400

    Python client for the Agave API, including modules for managing projects, files, and links.

---
+++ agave_api/__init__.py
@@ -0,0 +1,6 @@
+from .client import AgaveClient
+from .link import Link
+from .file_management import FileManagement
+from .project_management import ProjectManagement
+
+__all__ = ["AgaveClient", "Link", "FileManagement", "ProjectManagement"]
---
+++ agave_api/link.py
@@ -0,0 +1,41 @@
+class Link:
+    """
+    Handles authentication-related functionalities for the Agave API.
+    """
+
+    def __init__(self, agave_client):
+        self.agave_client = agave_client
+
+    def create(self, reference_id: str):
+        """
+        Creates a link token with an reference ID.
+
+        Args:
+            reference_id (str): A reference ID associated with the token.
+
+        Returns:
+            dict: The response containing the link token.
+        """
+        url = f"{self.agave_client.base_url}/link/token/create"
+        data = {"reference_id": reference_id}
+        response = self.agave_client.post(
+            url, headers=self.agave_client.headers, data=data
+        )
+        return response.json()
+
+    def exchange(self, public_token):
+        """
+        Exchanges a public token for an access token.
+
+        Args:
+            public_token (str): The public token to exchange.
+
+        Returns:
+            dict: The response containing the access token.
+        """
+        url = f"{self.agave_client.base_url}/link/token/exchange"
+        data = {"public_token": public_token}
+        response = self.agave_client.post(
+            url, headers=self.agave_client.headers, data=data
+        )
+        return response.json()
